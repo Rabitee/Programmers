ea68d243fa693c5dfe81cb6a3391f7b892e4e06e
DonghwaJeong <kbc1685@naver.com>
2020-03-30 10:49:06 +0900

    1

---
+++ StackQueue/1.py
@@ -0,0 +1,10 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42588
+
+def solution(heights):
+    answer = [0] * len(heights)
+    for i in range((len(heights) - 1), 0, -1):
+        for j in range(i - 1, -1, -1):
+            if heights[i] < heights[j]:
+                answer[i] = j + 1
+                break
+    return answer
