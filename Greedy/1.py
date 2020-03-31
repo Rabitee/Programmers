82e22e5c97a28d340f6240e074d30e745cd80e0a
DonghwaJeong <kbc1685@naver.com>
2020-03-31 10:31:17 +0900

    1

---
+++ Greedy/1.py
@@ -0,0 +1,14 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42862
+
+def solution(n, lost, reserve):
+    l = sorted(list(set(lost)-set(reserve)))
+    r = list(set(reserve) - set(lost))
+    answer = n - len(l)
+    for e in l:
+        if e-1 in r and e-1 not in l:
+            r.remove(e-1)
+            answer += 1
+        elif e+1 in r and e+1 not in l:
+            r.remove(e+1)
+            answer += 1
+    return answer
