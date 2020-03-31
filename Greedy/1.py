cc2f2478aaf9b589b00cc5137459ed4d76cbbd0b
DonghwaJeong <kbc1685@naver.com>
2020-03-31 10:33:53 +0900

    1

--- Greedy/1.py
+++ Greedy/1.py
@@ -5,10 +5,10 @@ def solution(n, lost, reserve):
     r = list(set(reserve) - set(lost))
     answer = n - len(l)
     for e in l:
-        if e-1 in r and e-1 not in l:
+        if e-1 in r:
             r.remove(e-1)
             answer += 1
-        elif e+1 in r and e+1 not in l:
+        elif e+1 in r:
             r.remove(e+1)
             answer += 1
     return answer
